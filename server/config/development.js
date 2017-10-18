4488c7bc45b5c6a4b926133cdab96b1fef2e82ba
Kenneth Phang <bunnyppl@hotmail.com>
2017-10-19 02:04:06 +0800

    for heroku

--- server/config/development.js
+++ server/config/development.js
@@ -3,5 +3,5 @@ const domain_name = "http://localhost:3000";
 module.exports = {
     domain_name: domain_name,
     mongodb: process.env.MONGODB_URL || "mongodb://nusiss:password1234@ds029456.mlab.com:29456/judgeformdb",
-    port: 3000,
+    port: process.env.PORT,
 }
