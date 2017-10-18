e9629b0631680970830b7d2efa8d46e5848c36c7
Kenneth Phang <bunnyppl@hotmail.com>
2017-10-19 01:57:16 +0800

    for heroku

--- server/config/production.js
+++ server/config/production.js
@@ -1,7 +1,7 @@
 'use strict';
-const domain_name = "http://localhost:3000";
+const domain_name = "https://young-spire-24679.herokuapp.com/";
 module.exports = {
     domain_name: domain_name,
-    mongodb: process.env.MONGODB_URL,
+    mongodb: process.env.MONGODB_URL || "mongodb://nusiss:password1234@ds029456.mlab.com:29456/judgeformdb",
     port: 3000,
 }
